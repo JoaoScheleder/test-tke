cdef291dbc4fd66cebd361b2fcd87dd01ea3e61f
JoaoScheleder <jg.scheleder@hotmail.com>
2025-06-29 10:36:45 -0300

    Refactor code structure for improved readability and maintainability

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, FormsModule ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
@@ -13,7 +14,8 @@ export class Header {
   
   is_fullscreen = false;
   is_dark_mode = false;
-  serch = '';
+  search = '';
+
 
   constructor() {}
 
