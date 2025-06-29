f6600fdd1d45d57f6987dbe546fa979845b5a482
JoaoScheleder <jg.scheleder@hotmail.com>
2025-06-29 17:34:26 -0300

    Implement movie details component with data retrieval; add loading state and error handling

--- src/app/pages/movie-details/movie-details.ts
+++ src/app/pages/movie-details/movie-details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MOVIE_SERVICE } from '../../../interface/movie-service';
+import { ActivatedRoute } from '@angular/router';
+import { IMovie } from '../../../interface/movie';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,6 +9,41 @@ import { Component } from '@angular/core';
   templateUrl: './movie-details.html',
   styleUrl: './movie-details.scss'
 })
-export class MovieDetails {
+export class MovieDetails implements OnInit {
+
+  private movieService = inject(MOVIE_SERVICE);
+  private movieId: number | null = null;
+  
+  movie : IMovie | undefined = undefined;
+  loading: boolean = true;
+
+  constructor(private activatedRoute : ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.movieId = Number(this.activatedRoute.snapshot.params['id']);
+    this.getMovieById();
+  }
+
+  getMovieById(){
+    if (this.movieId) {
+      this.loading = true;
+      this.movieService.getMovieById(this.movieId).subscribe({
+        next: (movie) => {
+          if (movie) {
+            this.movie = movie;
+            console.log('Movie details:', movie);
+          } else {
+            console.error('Movie not found');
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching movie details:', err);
+        },
+        complete: () => {
+          this.loading = false;
+        }
+      })
+    }
+  }
 
 }
