78e9aa80ff223c300744b08aa627ea7dff112e9b
JoaoScheleder <jg.scheleder@hotmail.com>
2025-07-01 12:33:11 -0300

    feat: implement MovieHTTPService and configure environment for API integration

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,18 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { MOVIE_SERVICE } from '../interface/movie-service';
-import { MovieMockService } from './services/mock/movie-mock-service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { MovieHTTPService } from './services/http/movie-http-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(BrowserAnimationsModule, BrowserAnimationsModule),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(routes),
     {
       provide: MOVIE_SERVICE,
-      useClass: MovieMockService,
+      useClass: MovieHTTPService,
     },
   ]
 };
--- client/src/app/services/http/movie-http-service.ts
+++ client/src/app/services/http/movie-http-service.ts
@@ -2,21 +2,25 @@ import { Injectable } from '@angular/core';
 import { MovieService } from '../../../interface/movie-service';
 import { Observable } from 'rxjs';
 import { IMovie } from '../../../interface/movie';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieHTTPService implements MovieService {
 
-  constructor() { }
+  api = environment.api + '/movies';
+
+  constructor(private http : HttpClient) { }
   
   getMovies(): Observable<IMovie[]> {
-    throw new Error('Method not implemented.');
+    return this.http.get<IMovie[]>(this.api);
   }
   getMovieById(id: number): Observable<IMovie | undefined> {
-    throw new Error('Method not implemented.');
+    return this.http.get<IMovie | undefined>(`${this.api}/${id}`);
   }
   getMoviesByTitle(title: string): Observable<IMovie[]> {
-    throw new Error('Method not implemented.');
+    return this.http.get<IMovie[]>(`${this.api}/title/${title}`);
   }
 }
